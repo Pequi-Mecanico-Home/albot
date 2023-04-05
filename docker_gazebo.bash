13efb4a267220b8d087d7b1db290873267632533
José Rafael <joserafaelrebelo@gmail.com>
2023-04-05 00:54:45 -0300

    fix(albot): fix gazebo low fps

--- docker_gazebo.bash
+++ docker_gazebo.bash
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-xhost +local:nvidia-docker && \
-nvidia-docker run -it --rm --env="DISPLAY" --gpus all --env="QT_X11_NO_MITSHM=1" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="LIBGL_ALWAYS_SOFTWARE=1" --cpus 4  --memory 4g gazebo-albot:1.1
+xhost +local:
+docker run \
+        -it \
+        --rm \
+        --privileged \
+        --env="DISPLAY" \
+        --gpus all \
+        --env="QT_X11_NO_MITSHM=1" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --cpu-shares 1024  \
+        --memory 2g \
+        gazebo-albot:1.1
+
+        # --env="LIBGL_ALWAYS_SOFTWARE=1" \
