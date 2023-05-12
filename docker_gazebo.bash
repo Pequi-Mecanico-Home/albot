763a8dd1a0a3a68a8acf625767763f8e4e7bd6e8
José Rafael <joserafaelrebelo@gmail.com>
2023-05-12 18:33:59 -0300

    chore(albot-description): Aldo fixxx

--- docker_gazebo.bash
+++ docker_gazebo.bash
@@ -5,9 +5,13 @@ docker run \
         -it \
         --rm \
         --privileged \
-        --env="DISPLAY" \
-        --env="QT_X11_NO_MITSHM=1" \
-        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --net=host \
+        --env DISPLAY="$DISPLAY" \
+        --env QT_X11_NO_MITSHM=1 \
+        --env LIBGL_ALWAYS_SOFTWARE=0 \
+        --volume "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
         --cpu-shares 1024  \
         --memory 2g \
         gazebo-rviz-albot:1.2
+
+
