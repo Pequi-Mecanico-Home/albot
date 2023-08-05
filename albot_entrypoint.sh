4e10dde1f8e0b6d46558ce5526f4e55f8d6d5576
Nigel Dias <nigeldias2012@gmail.com>
2023-08-05 18:46:59 -0300

    feature(albot_entrypoint.sh) adding apriltags models to gazebo models path

--- albot_entrypoint.sh
+++ albot_entrypoint.sh
@@ -6,7 +6,7 @@ colcon build --symlink-install
 
 source install/setup.bash
 
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$ALBOT_WS/src/aws-robomaker-small-warehouse-world/models/
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$ALBOT_WS/src/aws-robomaker-small-warehouse-world/models/:$ALBOT_WS/src/albot-description/models/
 chmod 777 -R $ALBOT_WS/src
 
 exec "$@"
